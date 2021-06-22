1a53853995af9c45a6fa34288e89014b8ba7750b
FernandoMelendez25 <00018720@uca.edu.sv>
2021-06-22 10:58:50 -0600

    First commit

---
+++ ProyectoFinalPOOBD/ProyectoFinalPOOBD/Views/frmMainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ProyectoFinalPOOBD.Views
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
