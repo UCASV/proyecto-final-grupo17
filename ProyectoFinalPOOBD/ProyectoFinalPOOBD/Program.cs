3e4efdd4ede7e27dc6b9f364074c79212f52ec57
Samuel581 <00023020@uca.edu.sv>
2021-06-22 15:14:53 -0600

    Front end base

--- ProyectoFinalPOOBD/ProyectoFinalPOOBD/Program.cs
+++ ProyectoFinalPOOBD/ProyectoFinalPOOBD/Program.cs
@@ -18,7 +18,7 @@ namespace ProyectoFinalPOOBD
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMainForm());
+            Application.Run(new frmLogin());
         }
     }
 }
