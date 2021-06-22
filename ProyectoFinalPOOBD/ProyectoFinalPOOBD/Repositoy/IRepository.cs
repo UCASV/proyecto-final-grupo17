96503adcff2460fedae224d1a1a8d9e2d8b8e608
FernandoMelendez25 <00018720@uca.edu.sv>
2021-06-22 16:17:49 -0600

    Add: Repository

---
+++ ProyectoFinalPOOBD/ProyectoFinalPOOBD/Repositoy/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalPOOBD.Repositoy
+{
+    interface IRepository<T>
+    {
+        List<T> GetAll();
+
+        void Create(T item);
+
+        void Update(T item);
+
+        void Delete(T item);
+
+        T Find(int pk);
+    }
+}
