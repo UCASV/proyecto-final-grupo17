e4fe51e02fa25ab5b77c2591c1f9d9aceed9b328
KevinMejia24 <00058620@uca.edu.sv>
2021-06-24 16:58:17 -0600

    Agregando la lista de enfermedades

---
+++ ProyectoFinalPOOBD/ProyectoFinalPOOBD/ViewModel/SideEffectVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalPOOBD.ViewModel
+{
+    public class SideEffectVM
+    {
+        public string Effect { get; set; }
+        public int Amount { get; set; }
+
+        public SideEffectVM()
+        {
+
+        }
+
+        public SideEffectVM(string Effect, int Amount)
+        {
+            this.Effect = Effect;
+            this.Amount = Amount;
+        }
+        
+    }
+}
