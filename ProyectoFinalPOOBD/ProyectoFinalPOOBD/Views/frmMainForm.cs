07deb8be0306e7e1f2f9c252fb05212d1bc30ab1
FernandoMelendez25 <00018720@uca.edu.sv>
2021-06-25 09:57:58 -0600

    Cleaning frmMainForm

--- ProyectoFinalPOOBD/ProyectoFinalPOOBD/Views/frmMainForm.cs
+++ ProyectoFinalPOOBD/ProyectoFinalPOOBD/Views/frmMainForm.cs
@@ -3,19 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
-using iText.IO.Font.Constants;
-using iText.Kernel.Font;
-using iText.Kernel.Geom;
-using iText.Kernel.Pdf;
-using Microsoft.EntityFrameworkCore;
-using ProyectoFinalPOOBD.Models;
-using ProyectoFinalPOOBD.VaccineContext;
-using ProyectoFinalPOOBD.FunctionsMeanwhile;
-using ProyectoFinalPOOBD.Repository;
-using iText.Layout;
-using iText.Layout.Element;
-using Document = iText.Layout.Document;
 
 namespace ProyectoFinalPOOBD.Views
 {
@@ -24,36 +11,6 @@ namespace ProyectoFinalPOOBD.Views
         public frmMainForm()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, System.EventArgs e)
-        {
-           
-        }
-
-        private void CreatePdf()
-        {
-            SaveFileDialog pathDialog = new SaveFileDialog();
-            pathDialog.Filter = "PDF document (*.pdf)|*.pdf";
-            DialogResult response = pathDialog.ShowDialog();
-
-            if (response == DialogResult.OK)
-            {
-                
-                PdfWriter pdfW = new PdfWriter(pathDialog.FileName);
-                PdfDocument pdf = new PdfDocument(pdfW);
-                Document document = new Document(pdf, PageSize.LETTER);
-
-                document.SetMargins(60, 20, 55, 20);
-                var prueba = new UserServices().Find(1);
-                string detalle = "User: " + prueba.Username + Environment.NewLine;
-                detalle += "Password: " + prueba.Password;
-                Paragraph text = new Paragraph(detalle);
-
-                document.Add(text);
-                
-                document.Close();
-            }
         }
     }
 }
