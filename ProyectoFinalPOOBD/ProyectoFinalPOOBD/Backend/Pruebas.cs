5b82c0d5ed45c70f6cf21d04ba33d4956afb24dd
KevinMejia24 <00058620@uca.edu.sv>
2021-06-25 13:46:09 -0600

    Parte de la funcion para la estadistica de personas que han sido vacunadas

---
+++ ProyectoFinalPOOBD/ProyectoFinalPOOBD/Backend/Pruebas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProyectoFinalPOOBD.Backend;
+
+
+namespace ProyectoFinalPOOBD.Backend
+{
+    public partial class Pruebas : Form
+    {
+        public Pruebas()
+        {
+            InitializeComponent();
+        }
+
+        private void Pruebas_Load(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = Backend.Functions2.OneVaccination();
+        }
+    }
+}
