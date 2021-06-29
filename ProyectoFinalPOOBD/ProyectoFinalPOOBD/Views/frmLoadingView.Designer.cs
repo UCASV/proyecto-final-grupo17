33c10c02b9e5f0a6e89abfa0cf29b03a69464e02
Samuel Ortiz <00023020@uca.edu.sv>
2021-06-28 20:32:41 -0600

    Added a loading screen

---
+++ ProyectoFinalPOOBD/ProyectoFinalPOOBD/Views/frmLoadingView.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace ProyectoFinalPOOBD.Views
+{
+    partial class frmLoadingView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbar = new CircularProgressBar.CircularProgressBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblWaitMessage = new System.Windows.Forms.Label();
+            this.tmrSideEffects = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbar
+            // 
+            this.pbar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.pbar.AnimationSpeed = 500;
+            this.pbar.BackColor = System.Drawing.Color.Transparent;
+            this.pbar.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pbar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(124)))), ((int)(((byte)(177)))));
+            this.pbar.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(51)))), ((int)(((byte)(98)))));
+            this.pbar.InnerMargin = 2;
+            this.pbar.InnerWidth = -1;
+            this.pbar.Location = new System.Drawing.Point(200, 39);
+            this.pbar.MarqueeAnimationSpeed = 2000;
+            this.pbar.Name = "pbar";
+            this.pbar.OuterColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(20)))), ((int)(((byte)(39)))));
+            this.pbar.OuterMargin = -25;
+            this.pbar.OuterWidth = 26;
+            this.pbar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(124)))), ((int)(((byte)(177)))));
+            this.pbar.ProgressWidth = 15;
+            this.pbar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.pbar.SecondaryFont = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.pbar.Size = new System.Drawing.Size(257, 257);
+            this.pbar.StartAngle = 270;
+            this.pbar.Step = 5;
+            this.pbar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbar.SubscriptColor = System.Drawing.Color.Transparent;
+            this.pbar.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.pbar.SubscriptText = ".23";
+            this.pbar.SuperscriptColor = System.Drawing.Color.Transparent;
+            this.pbar.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.pbar.SuperscriptText = "°C";
+            this.pbar.TabIndex = 0;
+            this.pbar.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.pbar.Value = 68;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.pbar, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 266F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(659, 301);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // lblWaitMessage
+            // 
+            this.lblWaitMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblWaitMessage.AutoSize = true;
+            this.lblWaitMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(51)))), ((int)(((byte)(98)))));
+            this.lblWaitMessage.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblWaitMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(124)))), ((int)(((byte)(177)))));
+            this.lblWaitMessage.Location = new System.Drawing.Point(76, 325);
+            this.lblWaitMessage.Name = "lblWaitMessage";
+            this.lblWaitMessage.Size = new System.Drawing.Size(490, 64);
+            this.lblWaitMessage.TabIndex = 2;
+            this.lblWaitMessage.Text = "Paciente en observacion, anote todos los \r\nsintomas secundarios que pueda notar";
+            this.lblWaitMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tmrSideEffects
+            // 
+            this.tmrSideEffects.Enabled = true;
+            this.tmrSideEffects.Tick += new System.EventHandler(this.tmrSideEffects_Tick);
+            // 
+            // frmLoadingView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(51)))), ((int)(((byte)(98)))));
+            this.ClientSize = new System.Drawing.Size(659, 525);
+            this.Controls.Add(this.lblWaitMessage);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLoadingView";
+            this.Text = "frmLoadingView";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CircularProgressBar.CircularProgressBar pbar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblWaitMessage;
+        private System.Windows.Forms.Timer tmrSideEffects;
+    }
+}
---
+++ ProyectoFinalPOOBD/ProyectoFinalPOOBD/Views/frmLoadingView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinalPOOBD.Views
+{
+    public partial class frmLoadingView : Form
+    {
+        public frmLoadingView()
+        {
+            InitializeComponent();
+            pbar.Value = 0;
+
+        }
+
+        private void tmrSideEffects_Tick(object sender, EventArgs e)//Se usa el timer para incrimentar el porcentaje de la barra de carga circular
+
+        {
+            pbar.Value += 1;
+            pbar.Text = pbar.Value.ToString() + "%";
+            if (pbar.Value == 100)
+            {
+                tmrSideEffects.Enabled = false;
+                this.Close();
+                //TODO Aca iria para que muestre el formulario de efectos secundarios
+            }
+        }
+    }
+}
